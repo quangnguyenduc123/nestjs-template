232c5779edabf57d8f66118c896029f67ae020cf
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-10-06 16:46:18 +0700

    add get api

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -5,27 +5,31 @@ import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  public constructor(private readonly reflector: Reflector) {}
+  public constructor(private readonly reflector: Reflector) { }
   canActivate(context: ExecutionContext): boolean {
     const ctx = GqlExecutionContext.create(context);
-    const auth = ctx.getContext().req.headers.authorization;
-    console.log('info', ctx.getInfo())
-    console.log(auth)
-    ctx.getContext().req.user = {
+    try {
+      const auth = ctx.getContext().req.headers.authorization;
+      console.log('info', ctx.getInfo())
+      console.log(auth)
+      ctx.getContext().req.user = {
         id: '123',
         name: 'quang',
         role: 'User'
-    }
-    const isPublic = this.reflector.get<boolean>(
-      'isPublic',
-      context.getHandler(),
-    );
+      }
+      const isPublic = this.reflector.get<boolean>(
+        'isPublic',
+        context.getHandler(),
+      );
 
-    // If controller has Public decorator => pass not check jwt header
-    if (isPublic) {
+      // If controller has Public decorator => pass not check jwt header
+      if (isPublic) {
+        return true;
+      }
+      console.log('Auth Guard');
       return true;
+    } catch (error) {
+      return true
     }
-    console.log('Auth Guard');
-    return true;
   }
 }
